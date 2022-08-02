245b0bc610b4ab45dea4c2c1dba64a6a245ee892
Vishal Kulkarni <kulkarni.vishal@ltts.com>
2022-08-02 13:50:40 +0530

    Now created the store and also create the feature sections which consist of createslice method which consist of parameter name, initialstate, reducer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { configureStore } from "@reduxjs/toolkit";
+import { store } from "./App/store";
 import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const store = configureStore({
-  reducer :{
-
-  }
-})
 root.render(
   <React.StrictMode>
     <Provider store={store}>
